18f6c3110a5f6599be2a9d85ec552099b04f22a8
victor <victor@boyahq.com>
2024-09-18 12:31:31 +0300

    Updated Feddback list ui

--- src/presentation/components/FeedbackList.tsx
+++ src/presentation/components/FeedbackList.tsx
@@ -18,32 +18,47 @@ interface FeedbackListProps {
 
 const FeedbackList: React.FC<FeedbackListProps> = ({ psychologists }) => {
   return (
-    <Box sx={{ mt: 4 }}>
+    <Box>
       {psychologists.map((psychologist) => (
-        <Box key={psychologist.id} sx={{ mb: 2 }}>
+        <>
           <Box
+            key={psychologist.id}
             sx={{
-              display: "flex",
-              mb: 1,
-              alignItems: "center",
-              gap: 1,
+              mb: 2,
+              p: 2,
+              cursor: "pointer",
+              "&:hover": {
+                backgroundColor: "rgba(0, 0, 0, 0.1)",
+              },
             }}
           >
-            <Avatar alt="JM" src={psychologist.url}></Avatar>
-            <Typography variant="h6">{psychologist.name}</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                mb: 1,
+                alignItems: "center",
+                gap: 1,
+              }}
+            >
+              <Avatar alt="JM" src={psychologist.url}></Avatar>
+              <Box>
+                <Typography variant="h6">{psychologist.name}</Typography>
+                <Typography variant="subtitle1">
+                  Score: {psychologist.averageScore}
+                </Typography>
+              </Box>
+            </Box>
+
+            <List>
+              {psychologist.feedbacks.map((feedback) => (
+                <ListItem key={feedback.id}>
+                  <ListItemText primary={`- ${feedback.summary}`} />
+                </ListItem>
+              ))}
+            </List>
           </Box>
-          <Typography variant="subtitle1">
-            Score: {psychologist.averageScore}
-          </Typography>
-          <List>
-            {psychologist.feedbacks.map((feedback) => (
-              <ListItem key={feedback.id}>
-                <ListItemText primary={`- ${feedback.summary}`} />
-              </ListItem>
-            ))}
-          </List>
           <Divider />
-        </Box>
+        </>
       ))}
     </Box>
   );
