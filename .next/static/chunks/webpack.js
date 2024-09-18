4b76917fef776d966affc2bc290436174e655534
victor <victor@boyahq.com>
2024-09-18 09:57:34 +0300

    update readme and dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45b7e9de51259ef5"; }
+/******/ 		__webpack_require__.h = function() { return "901e14b5d7daf815"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
